4d909c17fc0ffa945b69c97991c8d916e7aaa93f
dahoon <kdh@skorstudio.com>
2025-01-25 15:44:40 +0900

    변경사항 설명

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import OrganizationTree from './generator';
 
 const App = () => {
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      {/* ... 라우트 설정 ... */}
+      <OrganizationTree />
     </Router>
   );
 };
